030029cc92d5e09533087dfff9ab1ff607f37c84
Pranav Kedar <pranavkedar2121@gmail.com>
2026-06-05 13:22:22 +0530

    Add program to print all pairs in an array

---
+++ Arrays/pairs_Array.java
@@ -0,0 +1,11 @@
+public class pairs_Array {
+    public static void main(String args[]){
+        int nums[] = {2,4,6,8,10};
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                System.out.print("("+nums[i]+" "+nums[j]+") ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
