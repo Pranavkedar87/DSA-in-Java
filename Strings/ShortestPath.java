4b6bf5b0ed56983d69e003a77b28649ca375aadd
Pranav Kedar <pranavkedar2121@gmail.com>
2026-06-11 12:04:45 +0530

    Give A Route Containing 4 Direction E,W,N,S . Find the shortest path to reach destination

---
+++ Strings/ShortestPath.java
@@ -0,0 +1,25 @@
+package Strings;
+import java.util.*;
+public class ShortestPath {
+    public static float distance(String s){
+        int x=0 , y=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == 'N' || s.charAt(i) == 'n'){
+            y++;
+        }else if(s.charAt(i) == 'E' || s.charAt(i) == 'e'){
+            x++;
+        }else if(s.charAt(i) == 'S' || s.charAt(i) == 's'){
+            y--;
+        }else{
+            x--;
+        }
+        }
+        x = x*x;
+        y = y*y;
+        return (float)Math.sqrt(x+y);
+    }
+    public static void main(String args[]){
+        String s = "WNEENESENNN";
+        System.out.println(distance(s));
+    }
+}
--- Strings/maxLengthBetweenEqualCharacters.java
+++ Strings/maxLengthBetweenEqualCharacters.java
@@ -19,8 +19,49 @@ public class maxLengthBetweenEqualCharacters {
         }
         return result-1;
     }
+    public static int check2(String s) {
+        int n = s.length();
+        int max = Integer.MIN_VALUE;
+        int result = 0;
+        int j=0;
+        int count =0;
+        for(int i=j+1;i<n-1;i++){
+                count++;
+                if(s.charAt(j) == s.charAt(i)){
+                    if(max < count){
+                        result = count;
+                        max = count;
+                    }
+                }
+                if((i+j) == (n-1) && j<(n-1)){
+                    j++;
+                }
+                if((i+j) == (n-1)){
+                    count=0;
+                }
+            }
+        return result-1;
+    
+        }
+        public static void shortestPalindrome(String s) {
+        String result ="";
+        int n = s.length();
+        int a[] = new int[n-1];
+        for(int i=0;i<n/2;i++){
+            if(s.charAt(i) != s.charAt((n-1)-i)){
+                result += 1;
+            }else{
+                a[i] = 0;
+            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.println(a[i]);
+        }
+    }
+
     public static void main(String args[]){
-        String s = "mgntdygtxrvxjnwksqhxuxtrv";
-        System.out.println(check(s));
+        String s = "aacecaaa";
+        //System.out.println(check2(s));
+        shortestPalindrome(s);
     }
 }
