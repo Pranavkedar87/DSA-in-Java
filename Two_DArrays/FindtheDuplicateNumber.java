2e90a5789b4b3ac060276f9f48e0aaaf5b1e181a
Pranav Kedar <pranavkedar2121@gmail.com>
2026-06-15 23:00:53 +0530

    Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
    There is only one repeated number in nums, return this repeated number.

---
+++ Two_DArrays/FindtheDuplicateNumber.java
@@ -0,0 +1,26 @@
+package Two_DArrays;
+
+public class FindtheDuplicateNumber {
+    public static int findDuplicate(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            max = Math.max(nums[i],max);
+            min = Math.min(nums[i],min);
+        }
+        int count[] = new int[max - min + 1];
+        for(int num : nums){
+            count[num - min]++;
+        }
+        for(int i=0;i<count.length;i++){
+            if(count[i] > 1){
+                return i+min;
+            }
+        }
+        return -1;
+    }
+    public static void main(String args[]){
+        int nums[] = {1,3,4,2,2};
+        System.out.println(findDuplicate(nums));
+    }
+}
