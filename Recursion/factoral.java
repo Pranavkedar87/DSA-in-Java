f61633c294d2933000a5902562a292532cd57d32
Pranav Kedar <pranavkedar2121@gmail.com>
2026-06-16 22:22:15 +0530

    Find the power of X

--- Recursion/factoral.java
+++ Recursion/factoral.java
@@ -7,7 +7,14 @@ public class factoral {
         }
         return n * fact(n-1);
     }
+    public static double myPow(double x, int n) {
+        if(n == 0){
+            return 1;
+        }
+        return x * myPow(x,n-1);
+    }
     public static void main(String args[]){
         System.out.println(fact(4));
+        System.out.println(myPow(2,10));
     }
 }
---
+++ Recursion/printXpower.java
@@ -0,0 +1,31 @@
+package Recursion;
+public class printXpower {
+    public static double myPow(double x, int n) {
+        long N = n;
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        return power(x, N);
+    }
+
+    private static double power(double x, long n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        double halfpower = power(x, n / 2);
+        double halfpowersq = halfpower * halfpower;
+
+        if (n % 2 != 0) {
+            halfpowersq *= x;
+        }
+
+        return halfpowersq;
+    }
+    public static void main(String args[]){
+        System.out.println(myPow(2,10));
+    }
+}
