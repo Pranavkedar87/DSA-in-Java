0125da9daa55a32bfce5fe21b85139e630c0e52b
Pranav Kedar <pranavkedar2121@gmail.com>
2026-06-19 00:50:46 +0530

    Find number that occure one's in the array

---
+++ Arrays/SingleNumber.java
@@ -0,0 +1,13 @@
+public class SingleNumber {
+    public static int Find(int nums[]){
+        int ans = 0;
+        for(int i = 0;i < nums.length; i++){
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+    public static void main(String args[]){
+        int nums[] = {4,1,2,1,2};
+        System.out.println(Find(nums));
+    }
+}
