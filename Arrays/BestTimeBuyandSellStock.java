38fca20a7d19568843b2c30cf86dc65d725c5a7a
Pranav Kedar <pranavkedar2121@gmail.com>
2026-06-21 23:22:46 +0530

    Return the maximum profit you can achieve from this transaction.

---
+++ Arrays/BestTimeBuyandSellStock.java
@@ -0,0 +1,19 @@
+public class BestTimeBuyandSellStock {
+    public static int bestTime(int prices[]){
+        int buyprice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for(int i=0;i<prices.length;i++){
+            if(buyprice < prices[i]){
+                int profit = prices[i] - buyprice;
+                maxProfit = Math.max(profit,maxProfit);
+            }else{
+                buyprice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String args[]){
+        int prices[] = {7,1,5,3,6,4};
+        System.out.println(bestTime(prices));
+    }
+}
