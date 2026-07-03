07cee021d0d4ebb3d77432d2ce07950e77483f91
Pranav Kedar <pranavkedar2121@gmail.com>
2026-07-03 21:43:43 +0530

    Add brute force solution for Container With Most Water problem(Array List)

---
+++ ArrayList/ContainerWithMostWaterBruteForce.java
@@ -0,0 +1,28 @@
+package ArrayList;
+import java.util.*;
+public class ContainerWithMostWaterBruteForce {
+    public static int mostWater(List<Integer> nums){
+        int maxwater = 0;
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=i+1;j<nums.size();j++){
+                int min = Math.min(nums.get(i),nums.get(j));
+                int water = min * (j - i);
+                maxwater = Math.max(water,maxwater);
+            }
+        }
+        return maxwater;
+    }
+    public static void main(String args[]){
+        List<Integer> nums = new ArrayList<>();
+        nums.add(1);
+        nums.add(8);
+        nums.add(6);
+        nums.add(2);
+        nums.add(5);
+        nums.add(4);
+        nums.add(8);
+        nums.add(3);
+        nums.add(7); 
+        System.out.println(mostWater(nums));
+    }
+}
