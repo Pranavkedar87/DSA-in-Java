cedb4e435b54977f30f9bedca9037bad0304e902
Pranav Kedar <pranavkedar2121@gmail.com>
2026-07-07 13:36:32 +0530

    An array list  is monotonic if it is either monotone increasing or monotone decreasing, by using single loop

--- ArrayList/MonotonicArrayListTypeTwo.java
+++ ArrayList/MonotonicArrayListTypeTwo.java
@@ -1,26 +1,20 @@
 package ArrayList;
-import java.util.*;
-//import Recursion.sumOfN_NaturalNo;
 
-public class MonotonicArrayListTypeOne {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MonotonicArrayListTypeTwo {
     public static Boolean isMonotonic(List<Integer> nums){
-        int ans = 0;
+        int inc = 0;
+        int dec = 0;
         for(int i=0;i<nums.size()-1;i++){
             if(nums.get(i) >= nums.get(i+1)){
-                ans++;
+                inc++;
+            }if(nums.get(i) <= nums.get(i+1)){
+                dec++;
             }
         }
-        if(ans == nums.size()-1){
-            return true;
-        }else{
-            ans = 0;
-        }
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums.get(i) <= nums.get(i+1)){
-                ans++;
-            }
-        }
-        if(ans == nums.size()-1){
+        if(inc == nums.size()-1 || dec == nums.size()-1){
             return true;
         }else{
             return false;
@@ -29,10 +23,9 @@ public class MonotonicArrayListTypeOne {
     public static void main(String args[]){
         List<Integer> nums = new ArrayList<>();
         nums.add(1);
-         nums.add(5);
+         nums.add(2);
           nums.add(2);
            nums.add(3);
         System.out.println(isMonotonic(nums));
-        
     }
 }
