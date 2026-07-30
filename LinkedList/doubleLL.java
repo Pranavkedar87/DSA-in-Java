581f3cf9d763af51ada2bd5ace34f3a99fc7a848
Pranav Kedar <pranavkedar2121@gmail.com>
2026-07-31 00:10:07 +0530

    feat(doubly-linked-list): implement insertion at beginning

---
+++ LinkedList/doubleLL.java
@@ -0,0 +1,39 @@
+package LinkedList;
+
+public class doubleLL {
+    public static class node {
+        int data ;
+        node next;
+        node prev;
+        public node(int data){
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    public static node head;
+    public void addElemnt(int data){
+        node newnode = new node(data);
+        if(head == null){
+            head = newnode;
+            return;
+        }
+        newnode.next = head;
+        head.prev = newnode;
+        head = newnode;
+    }
+    public void printll(){
+        node temp = head;
+        while(temp != null){
+            System.out.print(temp.data+"->");
+            temp = temp.next;
+        }
+    }
+    public static void main(String args[]){
+        doubleLL l1 = new doubleLL();
+        l1.addElemnt(1);
+        l1.addElemnt(4);
+        l1.addElemnt(7);
+        l1.printll();
+    }
+}
