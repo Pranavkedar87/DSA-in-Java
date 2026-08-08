aed46bd2b16563e8d8d9074ee2ada3b1f7197f7c
Pranav Kedar <pranavkedar2121@gmail.com>
2026-08-08 22:56:05 +0530

    feat(stack): implement valid parentheses using stack

---
+++ Stack/validParentheses.java
@@ -0,0 +1,36 @@
+package Stack;
+
+import java.util.Stack;
+
+public class validParentheses {
+    public static boolean check(String str){
+        Stack<Character> s = new Stack<>();
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) == '[' ||
+               str.charAt(i) == '{' ||
+               str.charAt(i) == '(' ){
+                s.push(str.charAt(i));
+            }else{
+                if(s.isEmpty()){
+                    return false;
+                }
+                if(s.peek() == '{' && str.charAt(i) == '}' ||
+                   s.peek() == '[' && str.charAt(i) == ']' ||
+                   s.peek() == '(' && str.charAt(i) == ')' ){
+                        s.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        if(s.isEmpty()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public static void main(String args[]){
+        String str = "()[]{}}";
+        System.out.println(check(str));
+    }
+}
